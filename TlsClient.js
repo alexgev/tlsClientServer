609ed255ee8bca52b6adc2f90ec1ea0c3e223873
Alexey Gevorkyan <gevorkyan.alexey@gmail.com>
2019-10-15 17:50:24 +0300

    first commit

---
+++ TlsClient.js
@@ -0,0 +1,138 @@
+const tls = require('tls');
+const fs = require('fs');
+const events = require('events');
+
+const keyPath = `../certs/client.key`;
+const certPath = `../certs/client.crt`;
+const caPath = `../certs/ca.pem`;
+
+
+class TlsClient extends events {
+  constructor(props = {}) {
+    if (!props.caPath || !props.keyPath || !props.certPath) throw new Error('caPath, keyPath and certPath is required');
+    super();
+    this._eol = '\n';
+    this.taskIdField = '_taskId';
+    this.host = props.host || '127.0.0.1';
+    this.port = props.port || 80;
+    this.ca = fs.readFileSync(props.caPath);
+    this.key = fs.readFileSync(props.keyPath);
+    this.cert = fs.readFileSync(props.certPath);
+    this.taskId = 0;
+    this.taskIdLimit = 65536;
+
+    this._initConnection();
+  }
+
+  _initConnection() {
+    this.tasksSet = new Set();
+    // this.messagesMap = new Map();
+    const options = {
+      ca: this.ca,
+      key: this.key,
+      cert: this.cert,
+      host: this.host,
+      port: this.port,
+      rejectUnauthorized: true,
+      requestCert: true
+    };
+    const socket = tls.connect(options);
+    this.socket = socket;
+    // process.stdin.pipe(socket);
+    this.socket.on('secureConnect', () => {console.log('successfully connected')});
+    this.socket.on('data', this._parseDataFromServer.bind(this));
+    this.socket.on('error', err => console.log('err', err));
+    this.socket.on('close', async () => {
+      this._flushAllTasks();
+      await this._delay(5 * 1000);
+      this._initConnection();
+    });
+  }
+
+  _flushAllTasks() {
+    for (let taskId of this.tasksSet.keys()) {
+      this.tasksSet.delete(taskId);
+      this._emitTaskComplete(taskId, 'Task flushed');
+    }
+  }
+
+  _parseDataFromServer(buff) {
+    // console.log('getCipher()', socket.getCipher());
+    let str = buff.toString();
+    let obj = {};
+    let jsons = str.split(this._eol);
+    for (let i = 0; i < jsons.length; i++) {
+      let json = jsons[i];
+      if (!json) continue;
+      try {
+        obj = JSON.parse(json);
+        let taskId = this._getTaskIdFromObj(obj);
+        this._emitTaskComplete(taskId, obj);
+      } catch (e) {
+        console.log('e', e);
+        // this.sendErrorMessage(socket, this._getTaskIdFromObj(obj), e);
+      }
+    }
+    // console.log('this', this);
+  }
+
+  async _sendDataMessage(data) {
+    let obj = {
+      data: data
+    };
+    let {message, taskId} = this._formatDataToSend(obj);
+    this.socket.write(message);
+    const result = await this._createTaskListener(taskId);
+    return result;
+  }
+
+  _formatDataToSend(data) {
+    let objToSend = data || {};
+    const taskId = this.getNewTaskId();
+    objToSend[this.taskIdField] = taskId;
+    return {message: `${JSON.stringify(objToSend)}${this._eol}`, taskId};
+  }
+  getNewTaskId() {
+    return ++this.taskId % this.taskIdLimit;
+  }
+  _getTaskIdFromObj(obj) {
+    if (!obj) throw new Error('Obj is required');
+    return obj[this.taskIdField];
+  }
+  async _createTaskListener(taskId) {
+    return await Promise.race([
+      new Promise(res => {
+        this.tasksSet.add(taskId);
+        this.once(`taskId:${taskId}`, res)
+      }),
+      this._delay(20 * 1000, true)
+    ])
+  }
+
+  _emitTaskComplete(taskId, result) {
+    console.log(`success taskId with result ${taskId}`, result);
+    this.tasksSet.delete(taskId);
+    this.emit(`taskId:${taskId}`, result);
+  }
+
+  async _delay(ms, throwTimeoutError, timeoutMessage = 'My timeout error') {
+    return await new Promise((res, rej) =>
+      (throwTimeoutError) ? setTimeout(rej, ms, timeoutMessage) : setTimeout(res, ms)
+    );
+  }
+
+  async sendData(data) {
+    // console.log('data', data);
+    if (!data || typeof data !== 'object') throw new Error('Data is required');
+    return await this._sendDataMessage(data);
+  }
+}
+
+const tlsClient = new TlsClient({host: '127.0.0.1', port: 8000, keyPath, certPath, caPath});
+
+;(async () => {
+  // for (let i = 0; i < 100000; i++) {
+  //   tlsClient.sendData({hello: 'bla'});
+  // }
+   await tlsClient.sendData({hello: 'bla'});
+})();
---
+++ TlsServer.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const tls = require('tls');
+const port = 8000 || process.env.PORT;
+
+const keyPath = '../certs/server1.key';
+const certPath = '../certs/server1.crt';
+const caPath = '../certs/ca.pem';
+
+class TlsServer extends tls.Server {
+  constructor(...props) {
+    super(...props);
+    this._eol = '\n';
+    this.taskIdField = '_taskId';
+    this.on('secureConnection', (socket) => {
+      console.log('connected');
+      this.listeningFunc(socket);
+      // socket.write('hello\n');
+    })
+    this.on('error', err => console.log('err', err));
+  }
+  listeningFunc(socket) {
+    socket.on('data', (data) => this._parseDataFromClient(socket, data));
+  }
+
+  _parseDataFromClient(socket, buff) {
+    // console.log('getCipher()', socket.getCipher());
+    let str = buff.toString();
+    let obj = {};
+    let jsons = str.split(this._eol);
+    for (let i = 0; i < jsons.length; i++) {
+      try {
+        let json = jsons[i];
+        if (!json) continue;
+        obj = JSON.parse(json);
+        console.log('objFromReq', obj);
+        if (!obj[this.taskIdField]) return this._badRequest(socket);
+        this._sendDataMessage(socket, this._getTaskIdFromObj(obj), 'success');
+      } catch (e) {
+        this._sendErrorMessage(socket, this._getTaskIdFromObj(obj), e);
+      }
+    }
+  }
+
+  _getTaskIdFromObj(obj) {
+    if (!obj || typeof obj !== 'object') throw new Error('Obj must be and object');
+    return obj[this.taskIdField];
+  }
+
+
+  _badRequest(socket, taskId, text = 'badRequest') {
+    this._sendErrorMessage(socket, taskId, text);
+  }
+
+  _sendDataMessage(socket, taskId, text = 'success') {
+    let obj = {
+      data: text
+    };
+    let message = this._formatDataToSend(taskId, obj);
+    socket.write(message);
+  }
+
+  _sendErrorMessage(socket, taskId, text = 'unknown error') {
+    console.log('error', text);
+    if (text.message) text = text.message;
+    let obj = {
+      error: text
+    };
+    let message = this._formatDataToSend(taskId, obj);
+    socket.write(message);
+  }
+
+  _formatDataToSend(taskId, data) {
+    let objToSend = data || {};
+    if (Number.isInteger(taskId)) objToSend._taskId = taskId;
+    return `${JSON.stringify(objToSend)}${this._eol}`;
+  }
+}
+
+
+const options = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath),
+  ca: fs.readFileSync(caPath),
+  requestCert: true,
+  rejectUnauthorized: true
+};
+const tlsServer = new TlsServer(options);
+tlsServer.listen(port, () => console.log(`bound on port ${port}`));
